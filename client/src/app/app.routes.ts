f29a7b29d5b36864b0396525636c3dca295d94f8
NecroBread <mariovyordanov@gmail.com>
2024-01-09 18:16:07 +0200

    add jobs page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home/home.component';
 import { SecondaryNavComponent } from './core/components/secondary-nav/secondary-nav.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: SecondaryNavComponent,
-    children: [{ path: '', component: HomePageComponent }],
+    children: [
+      { path: '', component: HomePageComponent },
+      { path: 'jobs', component: JobsComponent },
+    ],
   },
 ];
---
+++ client/src/app/pages/jobs/jobs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs',
+  standalone: true,
+  imports: [],
+  templateUrl: './jobs.component.html',
+  styleUrl: './jobs.component.less'
+})
+export class JobsComponent {
+
+}
