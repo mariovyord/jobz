2e0824a55f2c1ef2789e29d82d5a86801336c9c0
NecroBread <mariovyordanov@gmail.com>
2024-01-09 16:10:00 +0200

    add badge

--- client/src/app/core/components/secondary-nav/secondary-nav.component.ts
+++ client/src/app/core/components/secondary-nav/secondary-nav.component.ts
@@ -3,11 +3,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-secondary-nav',
   standalone: true,
-  imports: [MatToolbarModule, MatListModule, MatIconModule, RouterModule],
+  imports: [
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    RouterModule,
+    MatBadgeModule,
+  ],
   templateUrl: './secondary-nav.component.html',
   styleUrl: './secondary-nav.component.less',
 })
