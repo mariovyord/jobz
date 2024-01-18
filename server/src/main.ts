c9f3bf3b1b2d0d080bc5c56a0b287a8c0194d603
NecroBread <mariovyordanov@gmail.com>
2024-01-18 12:36:42 +0200

    renames

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   setupSwagger(app);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   app.enableCors();
 
   await app.listen(3000);
