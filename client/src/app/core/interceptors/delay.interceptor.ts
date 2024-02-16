12185b6e5a8cf68777e7f7b0ce507f2d6c4c129e
NecroBread <mariovyordanov@gmail.com>
2024-02-16 17:23:26 +0200

    update delay intercaptor

--- client/src/app/core/interceptors/delay.interceptor.ts
+++ client/src/app/core/interceptors/delay.interceptor.ts
@@ -1,13 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import {
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { delay, mergeMap } from 'rxjs/operators';
-import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 /*
  *
@@ -21,23 +20,19 @@ export class DelayInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     // Check if it's production mode
-    if (environment.production) {
+    if (!isDevMode()) {
       // If it is, bypass the delay and handle the request immediately
       return next.handle(req);
-    } else {
-      // If it's not production (i.e., development), apply a random delay
-      const delayDuration = Math.random() * 1000;
-      console.log(
-        '[HTTP Delay Library] HTTP request delayed by',
-        (delayDuration / 1000).toFixed(2),
-        'seconds'
-      );
-
-      return of(null).pipe(
-        // Use delay here before proceeding with the request
-        delay(delayDuration),
-        mergeMap(() => next.handle(req))
-      );
     }
+
+    // If it's not production (i.e., development), apply a random delay
+    const delayDuration = Math.random() * 1000;
+    console.log(
+      '[HTTP delay] HTTP request delayed by',
+      (delayDuration / 1000).toFixed(2),
+      'seconds'
+    );
+
+    return next.handle(req).pipe(delay(delayDuration));
   }
 }
